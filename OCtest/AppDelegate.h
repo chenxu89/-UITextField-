6702bcdfa5dc9c0c39e50ab76542f81ff0aff35f
chenxu89 <chenxu@chenxudeimac.local>
2016-08-26 14:03:01 +0800

    Initial Commit

---
+++ OCtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCtest
+//
+//  Created by chenxu on 16/8/26.
+//  Copyright © 2016年 chenxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCtest
+//
+//  Created by chenxu on 16/8/26.
+//  Copyright © 2016年 chenxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
